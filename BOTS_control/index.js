c266fbeb65d76a3f0787a0709ef1475fc13c537f
Gonçalo Abreu Correa Brito da Silva <goncalosilva@MacBook-Air.home>
2019-07-18 22:40:37 +0100

    First Commit: Added MINIONS Control Console and Bots

---
+++ BOTS_control/index.js
@@ -0,0 +1,114 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var xhr = new XMLHttpRequest();
+const server = require('http').createServer();
+const io = require('socket.io')(server);
+const fs = require('fs');
+const chalk = require('chalk');
+var bot_version;
+var send_command;
+//Object
+var bots;
+bots = {};
+//Parse Package details
+console.log("Checking Version...")
+const fileContents = fs.readFileSync('./package.json', 'utf8')
+try {
+  const data = JSON.parse(fileContents)
+  server_version = data["version"];
+} catch(err) {
+  console.error("Failed to check version")
+  console.error(err)
+}
+setup();
+
+
+function motd() {
+  console.log("===============================")
+  console.log("MINIONS Control Console")
+  console.log(server_version)
+  console.log("===============================")
+}
+
+function setup() {
+  //Check for updates with version
+  motd();
+  console.log("Starting server...")
+
+  cliconsole();
+  //Load Server
+  io.on('connection', client => {
+    //New connection
+    var clientIp = client.request.connection.remoteAddress;
+    console.log("[SERVER]: New connection from " + clientIp)
+
+    client.on('bot_states', data => {
+      id = data["id"];
+      //Add BOT to Object or login
+      if (bots.hasOwnProperty(id)) {
+        //Already registered
+        console.log("[SERVER]: Bot Connected: " + id)
+        //Connection true
+        bots[id]["connection"] = true;
+      } else {
+        console.log("[SERVER]: Registering " + id)
+        state = data["state"];
+        link = data["link"];
+        bots[id] = {"connection": true, "state": state, "link": link}
+      }
+    });
+
+    send_command = function(channel, cmd) {
+      client.emit(channel, cmd);
+    }
+
+    client.on('disconnect', () => {
+      console.log("[SERVER]: Client " + clientIp + " disconnected")
+    });
+  });
+
+  console.log("[SERVER]: Waiting for connections...");
+  server.listen(3000);
+}
+
+
+
+function cliconsole() {
+  // Get process.stdin as the standard input object.
+  var standard_input = process.stdin;
+
+  // Set input character encoding.
+  standard_input.setEncoding('utf-8');
+  // When user input data and click enter key.
+  standard_input.on('data', function (data) {
+
+      // User input exit.
+      if(data === 'shutdown\n'){
+          // Program exit.
+          process.exit();
+      } else if (data === 'list bots\n') {
+          list();
+      } else if (data.includes("disconnect -bot")) {
+          dataarg = data.split(/\s+/)[2];
+          console.log("[SERVER]: Disconnecting "+dataarg+"...");
+          bots[dataarg]["connection"] = false;
+          send_command(dataarg, "disconnect");
+      } else {
+          // Print user input in console.
+          console.log('Undefined command');
+      }
+  });
+}
+
+function list() {
+  console.log(chalk.grey("Registered BOTS:"))
+  /*
+  ID: BOT_SUPREME0 | State: CONNECTED - UNSET
+  */
+  Object.keys(bots).forEach(function(key) {
+    if (bots[key]["connection"] == true) {
+      console.log(chalk.grey("ID: " + key + " | State: ") + chalk.green("CONNECTED") + " - " + bots[key]["state"].toUpperCase() + chalk.grey(" | Links: " + bots[key]["link"]))
+    } else {
+      console.log(chalk.grey("ID: " + key + " | State: ") + chalk.red("DISCONNECTED") + " - " + bots[key]["state"].toUpperCase() + chalk.grey(" | Links: " + bots[key]["link"]))
+    }
+  });
+}
