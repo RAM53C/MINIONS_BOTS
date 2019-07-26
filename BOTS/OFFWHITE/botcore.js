be9f9cfc984b844f6484ab7c66ffb14156412f63
Gonçalo Abreu Correa Brito da Silva <goncalosilva@MacBook-Air.home>
2019-07-26 03:00:43 +0100

    Added Fetch link methods with proxy support and Proxies Tests

--- BOTS/OFFWHITE/botcore.js
+++ BOTS/OFFWHITE/botcore.js
@@ -13,3 +13,26 @@ exports.getState = function() {
     process.exit(1)
   }
 }
+
+//Missing dependencies, deactivated
+//Get Link
+exports.getMainLink = function(link, callback) {
+  console.log("Fetching " + link);
+  (async () => {
+    const browser = await puppeteer.launch({
+      // Launch chromium using a proxy server on port 9876.
+      // More on proxying:
+      //    https://www.chromium.org/developers/design-documents/network-settings
+      args: [ '--proxy-server=103.105.48.16:80' ]
+    });
+    const page = await browser.newPage();
+
+    await page.goto(link);
+    var html = await page.content();
+
+    console.log(html); /* No Problem Mate */
+
+    browser.close();
+  })();
+  //console.log(chalk.red("Failed to fetch: " + error))
+}
