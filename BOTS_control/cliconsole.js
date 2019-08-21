4a87dcf1db91843b4c5d6e876577501aa36e3485
Gonçalo Abreu Correa Brito da Silva <goncalosilva@MacBook-Air.home>
2019-08-21 13:09:15 +0100

    Added rejected links features <list rl, rl_warning, rl solve -all>, start and stop commands

--- BOTS_control/cliconsole.js
+++ BOTS_control/cliconsole.js
@@ -1,16 +1,39 @@
 const fs = require('fs');
 const chalk = require('chalk');
+const warning = chalk.keyword('orange');
 
 //Essentials for notifications
-var notifications = {"ready": "Ready, waiting for console"}
-exports.evalState = function(state) {
+var notifications = {"ready": "chalk.green('Ready, waiting for console')", "check": "Checking Links..."}
+exports.evalState = function(state, rl) {
+    if (Array.isArray(rl) && rl.length) {
+      console.log(warning("[SERVER][WARNING]: There are rejected links, please run 'list rl' to see them"))
+    }
     if (notifications.hasOwnProperty(state)) {
-      return notifications[state]
+      return eval(notifications[state])
     } else {
       return state
     }
 }
 
+exports.listrl = function(bots) {
+  console.log(chalk.grey("Rejected Links:"))
+  Object.keys(bots).forEach(function(key) {
+    rlss = bots[key]["rejected_links"]
+    if (Array.isArray(rlss) && rlss.length) {
+      rls = bots[key]["rejected_links"]
+      console.log(chalk.grey("ID: " + key))
+      console.log(chalk.grey("Rejected links: "))
+      rls.forEach(function(element) {
+        console.log(chalk.grey(element));
+      });
+      console.log(chalk.grey("=========================="))
+    } else {
+      console.log(chalk.grey("ID: " + key))
+      console.log(chalk.grey("Rejected links: None"))
+    }
+  });
+  console.log("To solve this issue you can run 'rl solve -all' or reset the links using 'set link' feature")
+}
 
 exports.list = function(bots) {
   console.log(chalk.grey("Registered BOTS:"))
@@ -51,15 +74,67 @@ exports.set_link = function(bots, target, link) {
   console.log("[SERVER]: Setting links for "+target+"...");
   try {
     if (bots.hasOwnProperty(target)) {
-      bots[target]["link"] = link
-      console.log("[SERVER]: " + chalk.green("Links set ("+target+")"))
+      console.log("[SERVER]: Links set on " + target)
       return true;
     } else {
       console.log("[SERVER]: " + target + " is not registered")
       return false;
     }
   } catch(e) {
-    console.error("[SERVER]: Unable to set link on " + target)
+    console.error("[SERVER]: Unable to set links on " + target)
+    console.error(chalk.red(e))
+    return false;
+  }
+}
+
+exports.botsrl = function(bots) {
+  console.log("[SERVER]: Removing rejected links...")
+  rlbots = []
+  Object.keys(bots).forEach(function(key) {
+    rlss = bots[key]["rejected_links"]
+    if (Array.isArray(rlss) && rlss.length) {
+      rlbots.push(key)
+    }
+  });
+  return rlbots;
+}
+
+exports.check_start = function(bots, dataarg) {
+  console.log("[SERVER]: Starting "+dataarg+"...");
+  try {
+    if (bots.hasOwnProperty(dataarg)) {
+      if (!bots[dataarg]["state"] == "ready") {
+        console.log("[SERVER]: Unable to start " + dataarg + ": Current state is <"+bots[dataarg]["state"]+"> and not <ready>")
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      console.log("[SERVER]: " + dataarg + " is not registered")
+    }
+  } catch(e) {
+    console.error("[SERVER]: Unable to start " + dataarg)
+    console.error(chalk.red(e))
+    return false;
+  }
+}
+
+
+exports.check_stop = function(bots, dataarg) {
+  console.log("[SERVER]: Stopping "+dataarg+"...");
+  try {
+    if (bots.hasOwnProperty(dataarg)) {
+      if (!bots[dataarg]["state"] == "working") {
+        console.log("[SERVER]: Unable to stop " + dataarg + ": Current state is <"+bots[dataarg]["state"]+"> and not <ready>")
+        return false;
+      } else {
+        return true;
+      }
+    } else {
+      console.log("[SERVER]: " + dataarg + " is not registered")
+    }
+  } catch(e) {
+    console.error("[SERVER]: Unable to stop" + dataarg)
     console.error(chalk.red(e))
     return false;
   }
--- BOTS_control/index.js
+++ BOTS_control/index.js
@@ -5,11 +5,18 @@ const io = require('socket.io')(server);
 const cliCommands = require('./cliconsole');
 const fs = require('fs');
 const chalk = require('chalk');
+const warning = chalk.keyword('orange');
 var bot_version;
 var send_command;
 //Object
 var bots;
 bots = {};
+
+//Replace All Function
+String.prototype.replaceAll = function(str1, str2, ignore)
+{
+    return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
+}
 
 //Not very secure but fuck it
 process.on('uncaughtException', function (err) {
@@ -58,20 +65,42 @@ function setup() {
         //Already registered
         if (bots[id]["connection"] == false) {
           console.log("[SERVER]: Bot Connected: " + id)
+          if (Array.isArray(link) && link.length && data["state"] == "unset") {
+            console.log("[SERVER]: Links Already Loaded, sending <check_link> command")
+            linls = JSON.stringify(link)
+            linls = linls.replaceAll("\"", "'");
+            send_command(id, "check_link " + linls);
+          }
         }
         //Connection true
         if (bots[id]["state"] != data["state"]) {
-          stt = cliCommands.evalState(data["state"])
+          stt = cliCommands.evalState(data["state"], data["rejected_links"])
           console.log("[SERVER]["+id+"]: " + stt)
         }
+
+        if (data["state"] == "shutdown") {
+          console.log(warning("[SERVER][WARNING]: Unexpected shutdown of bot: " + id))
+          if (cliCommands.check_disconnect(bots, id)) {
+            send_command(id, "disconnect");
+          }
+        }
+
         state = data["state"];
         link = data["product_url"];
-        bots[id] = {"connection": true, "state": state, "link": link}
+        rl = data["rejected_links"];
+        bots[id] = {"connection": true, "state": state, "link": link, "rejected_links": rl}
       } else {
         console.log("[SERVER]: Registering " + id)
         state = data["state"];
         link = data["product_url"];
-        bots[id] = {"connection": true, "state": state, "link": link}
+        rl = data["rejected_links"];
+        bots[id] = {"connection": true, "state": state, "link": link, "rejected_links": rl}
+        if (Array.isArray(link) && link.length && bots[id]["state"] == "unset") {
+          console.log("[SERVER]: Links Already Loaded, sending <check_link> command")
+          linls = JSON.stringify(link)
+          linls = linls.replaceAll("\"", "'");
+          send_command(id, "check_link " + linls);
+        }
       }
     });
 
@@ -99,12 +128,24 @@ function cliconsole() {
   // When user input data and click enter key.
   standard_input.on('data', function (data) {
 
+
       // User input exit.
       if(data === 'shutdown\n'){
           // Program exit.
           process.exit();
       } else if (data === 'list bots\n') {
           cliCommands.list(bots);
+      } else if (data === 'list rl\n') {
+        cliCommands.listrl(bots);
+      } else if (data === 'rl solve -all\n') {
+        botsrl = cliCommands.botsrl(bots);
+        if (botsrl) {
+          botsrl.forEach(function(element) {
+            send_command(element, "rlsolve");
+          });
+        } else {
+          console.log("There aren't any rejected links")
+        }
       } else if (data.includes("disconnect -bot")) {
           dataarg = data.split(/\s+/)[2];
           if (cliCommands.check_disconnect(bots, dataarg)) {
@@ -116,9 +157,21 @@ function cliconsole() {
           if (cliCommands.set_link(bots, dataarg, dataarg2)) {
             send_command(dataarg, "check_link " + dataarg2);
           }
+      } else if (data.includes("start -bot")) {
+        dataarg = data.split(/\s+/)[2];
+        if (cliCommands.check_start(bots, dataarg)) {
+          send_command(dataarg, "start");
+        }
+      } else if (data.includes("stop -bot")) {
+        dataarg = data.split(/\s+/)[2];
+        if (cliCommands.check_stop(bots, dataarg)) {
+          send_command(dataarg, "stop");
+        }
       } else {
           // Print user input in console.
           console.log('Undefined command');
       }
+
+
   });
 }
