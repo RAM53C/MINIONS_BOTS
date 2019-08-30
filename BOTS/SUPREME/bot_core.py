1b31d6379ccfbf6ee1c20a9aea6cdb336c07091c
Gonçalo Abreu Correa Brito da Silva <goncalosilva@MacBook-Air.home>
2019-08-30 19:18:58 +0100

    Added SUPREME BOT, waiting for patch due to an error

---
+++ BOTS/SUPREME/bot_core.py
@@ -0,0 +1,289 @@
+import json
+from bs4 import BeautifulSoup
+import time
+import random
+import re
+import datetime
+import threading
+import subprocess
+import sys
+import os
+import requests
+import numpy as np
+global i
+global pmodelsset
+global products_math
+global p_models
+global keys
+global threads
+global working
+#For Links Setup
+global linkcheck
+global ready
+i = 0
+working = False
+pmodelsset = False
+keysdefined = False
+linkcheck = False
+ready = False
+
+# Enable/Disable Console Logs
+logs = True
+
+def logToConsole(text):
+    if logs == True:
+        print(text)
+        sys.stdout.flush()
+
+#Check Internet Connection
+#@yasinkuyu 08/09/2017
+
+def check_internet():
+    url='http://www.google.com/'
+    timeout=5
+    try:
+        _ = requests.get(url, timeout=timeout)
+        return True
+    except requests.ConnectionError:
+        return False
+
+
+#BOT Parser
+def cnf_parser(): #Refresh time: 1 second
+    global keys
+    logToConsole("Loading Parser...")
+    cp = threading.currentThread()
+    while getattr(cp, "do_run", True):
+        config_file = open('config.json')
+        keys = json.load(config_file)
+        #Conditions
+        cnf_process(keys)
+        time.sleep(1)
+
+def update_cnf(cnf):
+    myCmd = 'python cnf_modifier.py -c "' + str(cnf) + '"'
+    logToConsole("Updating Config: " + str(myCmd))
+    os.system(myCmd)
+
+def removeProduct(link):
+    global keys
+    buylinks = keys["product_url"];
+    print("Product was aquired, removing link from list...")
+    buylinks.remove(link)
+    # Remove product link from list
+    cnfdict = {"product_url": buylinks}
+    update_cnf(cnfdict)
+    if not buylinks: #Check if array is empty
+        print("Bot finished, no links to check")
+        cnfdict = {"state": "stop"}
+        update_cnf(cnfdict)
+
+
+
+def cnf_process(keys):
+    global oldkeys
+    global keysdefined
+    global linkcheck
+    global ready
+    global working
+    if (keysdefined == True and oldkeys != keys) or keysdefined == False:
+        oldkeys = keys
+        if keys["state"] == "shutdown" and keysdefined == True:
+            logToConsole("Parser recieved shutdown")
+            global cpw
+            cpw.do_run = False
+            sys.exit(1)
+        if keysdefined == False:
+            logToConsole("Parser state value is " + keys["state"] + " but its the first time, changing state")
+            cnfdict = {"state": "unset"}
+            update_cnf(cnfdict)
+        if keys["state"] == "unset":
+            logToConsole("Link Unset, waiting for control console...")
+        if keys["state"] == "check":
+            linkcheck = check_links(keys);
+        if keys["state"] == "ready":
+            if linkcheck:
+                linkcheck = False
+                ready = True
+                logToConsole("Ready - waiting for console")
+            else:
+                logToConsole("Error - Links not checked")
+                cnfdict = {"state": "unset"}
+                update_cnf(cnfdict)
+        if keys["state"] == "start":
+            if ready == True:
+                #Init BOT
+                ready = False
+                logToConsole("Initializing BOT...")
+                cnfdict = {"state": "working"}
+                update_cnf(cnfdict);
+                initialize_bot();
+            else:
+                logToConsole("Error while starting: State <ready> required")
+                cnfdict = {"state": "unset"}
+                update_cnf(cnfdict)
+        if keys["state"] == "stop":
+            if working == True:
+                #Stop BOT
+                logToConsole("Stopping BOT...")
+                linkcheck = True
+                stop_bot();
+                cnfdict = {"state": "ready"}
+                update_cnf(cnfdict);
+            else:
+                logToConsole("Error while stoping: State <working> not defined")
+                cnfdict = {"state": "unset"}
+                update_cnf(cnfdict)
+        #Code for keysdefined
+        if keysdefined == False:
+            keysdefined = True
+        #####################
+
+def check_links(keys):
+    logToConsole("Checking Links...")
+    #Validate links
+    buylinks = keys["product_url"];
+    if buylinks:
+        blv = []
+        rpl = []
+        for link in buylinks: # Just check domain
+            if ("https://www.supremenewyork.com/shop" in link):
+                logToConsole(str(link) + " OK!")
+                blv.append(link)
+            else:
+                logToConsole(str(link) + " FAILED!")
+                rpl.append(link)
+        if (blv):
+            #Export BLV and create config
+            cnfdict = {"product_url": blv, "rejected_links": rpl, "state": "ready"}
+            update_cnf(cnfdict)
+            return True
+        else:
+            #Export BLV and create config
+            cnfdict = {"product_url": blv, "rejected_links": rpl, "state": "unset"}
+            update_cnf(cnfdict)
+            return False
+    else:
+        logToConsole("No Links to check")
+        cnfdict = {"state": "unset"}
+        update_cnf(cnfdict)
+        return False
+
+def initialize_bot():
+    global working
+    if working == True:
+        logToConsole("BOT Instance already running")
+        logToConsole("Set state to <stop> first")
+    else:
+        working = True
+        global threads
+        global bw
+        bw = threading.Thread(target=MainCheck);
+        bw.start();
+
+def stop_bot():
+    global bw
+    bw.do_run = False
+
+def setup():
+    global searchbar
+    searchbar = [];
+    logToConsole("CHECK_CONNECTION")
+    if check_internet:
+        logToConsole("CONNECTED")
+    else:
+        logToConsole("CONNECTION_ERROR")
+        logToConsole("Failed to connect")
+        os.system("python cnf_modifier.py -c \"{'state':'shutdown'}\"")
+        sys.exit(1)
+
+def loadLink(link):
+    r = requests.get(link)
+    page_html = r.text;
+    soup = BeautifulSoup(page_html, "html.parser")
+    productname = soup.find_all('h1', {'class':"protect"})[0];
+    productname = str(productname.contents[0]);
+    productstate = soup.find_all('fieldset', {'id':"add-remove-buttons"})[0];
+    productstate = productstate.findChildren("input" , recursive=False)
+    productprice = soup.find_all('p', {'class':"price"})[0];
+    productprice = str(productprice.contents[0]);
+    sizes = []
+    if productstate:
+        product_available = True
+        # Get available sizes
+        productsizes = soup.find_all('select', {'id':"size"})[0];
+        productsizes = productstate.findChildren("option" , recursive=True)
+        for size in productsizes:
+            size = str(size.contents[0]);
+            sizes.append(size)
+    else:
+        product_available = False
+    product = {
+    "name": productname,
+    "state": product_available,
+    "price": productprice,
+    "sizes": sizes
+    }
+    return product
+
+
+def LinkRequest(link):
+    global keys
+    #Get link
+    product = loadLink(str(link))
+    #Strip Link
+    if (product["state"]):
+        ConsoleProductLog(product["name"], "Available for buy", product["sizes"])
+        # Check Sizes
+        sizeconfig = keys["buyoptions"][link];
+        buysizes = {}
+        for size in product["sizes"]:
+            if size in sizeconfig:
+                # Get sizes
+                buysizes[size] = sizeconfig[size]
+        if buysizes:
+            print("Buying Stocks...")
+            buyProduct(link, product["name"], product["price"], buysizes);
+            removeProduct(link)
+    else:
+        ConsoleProductLog(product["name"], "Not Available", product["sizes"])
+    time.sleep(1)
+
+def ConsoleProductLog(product, state, sizes):
+    now = datetime.datetime.now()
+    print("=========================")
+    print("Product: " + str(product))
+    print("State: " + str(state))
+    if sizes:
+        print("Available Sizes: " + str(sizes))
+    print("Checked at: " + now.strftime("%H:%M:%S"))
+    print("=========================")
+
+
+def MainCheck():
+    global keys
+    logToConsole("BOT: Starting...")
+    buylinks = keys["product_url"];
+    bws = threading.currentThread()
+    while getattr(bws, "do_run", True):
+        for link in buylinks:
+            LinkRequest(link); #Link Request
+
+def buyProduct(link, name, price, size):
+    print("*********************")
+    print("Order Info:")
+    print("*********************")
+    print("Product Name: " + str(name))
+    print("Product Link: " + str(link))
+    print("Quantity: ")
+    for key, value in size.items():
+        print(" - " + key + ": " + str(value) + " x " + str(price))
+
+
+if __name__ == '__main__':
+    setup();
+    global cpw
+    global threads
+    threads = []
+    cpw = threading.Thread(target=cnf_parser);
+    cpw.start();
---
+++ BOTS/SUPREME/main.py
@@ -0,0 +1,166 @@
+# dependencies
+import sys
+import os
+import json
+import inquirer
+import socketio
+import time
+import threading
+import subprocess
+
+# Setup Questions
+connectiontypeask = [
+    inquirer.List('contype',
+        message="Control Console Connection Type:",
+        choices=['localhost', 'remote']
+    )
+]
+
+ipask = [
+    inquirer.Text('ip', message="Control Console Connection IP:")
+]
+
+portask = [
+    inquirer.Text('port', message="Control Console Connection Port:")
+]
+
+
+# Sub-modules
+
+#BOT Parser
+def cnf_update(): #Refresh time: 2 seconds
+    global sio
+    print("Loading Bot Parser...")
+    cp = threading.currentThread()
+    while getattr(cp, "do_run", True):
+        state = getState();
+        sio.emit('bot_states', state)
+        time.sleep(3)
+
+def CheckVersion():
+    #Read file
+    print("Checking Version...")
+    config_file = open('config.json')
+    keys = json.load(config_file)
+    try:
+        return keys["version"]
+    except Exception as e:
+        print("Failed to check version:")
+        print(e)
+        sys.exit(1)
+
+def getState():
+    try:
+        config_file = open('config.json')
+        config = json.load(config_file)
+    except Exception as e:
+        print("Failed to parse config.json")
+        print(e)
+        sys.exit(1)
+    return config
+
+# Main Functions
+
+def setup():
+    global bot_version
+    bot_version = CheckVersion();
+    # Conection SETUP
+    # Questions
+    print("Connection SETUP:")
+    contype = inquirer.prompt(connectiontypeask)["contype"]
+    if contype == "remote":
+        ip = inquirer.prompt(ipask)["ip"]
+        port = inquirer.prompt(portask)["port"]
+    else:
+        ip = "localhost"
+        port = inquirer.prompt(portask)["port"]
+    cadress = str(ip) + ":" + str(port)
+    motd();
+    os.system("python cnf_modifier.py -c \"{'state':'unset'}\"")
+    initialize_bot();
+    main(cadress);
+
+def motd():
+    global bot_version
+    print("===============================")
+    print("MINIONS: SUPREME Bot")
+    print(str(bot_version))
+    print("===============================")
+
+def initialize_bot():
+    print("Initializing Bot...")
+    os.system("xterm -e \"python3 bot_core.py\" &")
+
+def stop_bot():
+    print("Stopping Bot...")
+    os.system("python cnf_modifier.py -c \"{'state':'shutdown'}\"")
+
+def main(ip):
+    global sio
+    print("Waiting for control console with adress: "+ip+"...")
+    sio = socketio.Client();
+
+    @sio.event
+    def connect():
+        global cpw
+        print("Connected to the control console!")
+        print("Initializing state update...")
+        threads = []
+        cpw = threading.Thread(target=cnf_update);
+        cpw.start();
+
+    @sio.event
+    def disconnect():
+        global cpw
+        print("Disconnected from the control console!")
+        cpw.do_run = False
+        print("Closing Socket...")
+        sio.disconnect()
+        main(ip);
+
+    def init_connect():
+        try:
+            sio.connect('http://' + ip)
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            if str(exc_obj) == "Connection refused by the server":
+                #Wait 2 seconds and try again
+                time.sleep(2)
+                init_connect()
+            else:
+                fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+                print("Error on SIO: ", exc_type, exc_obj, exc_tb.tb_lineno)
+
+    init_connect();
+    state = getState();
+    ids = state["id"]
+
+    @sio.on(ids)
+    def on_message(data):
+        print("Command Received: " + data)
+        if data == "disconnect":
+            global cpw
+            cpw.do_run = False
+            print("Closing Socket...")
+            sio.disconnect()
+            stop_bot()
+            sys.exit(1)
+        elif "check_link" in data:
+            print("Checking links...")
+            checklinks = data.split()[1]
+            os.system("python cnf_modifier.py -c \"{'product_url':"+checklinks+", 'state':'check'}\"")
+        elif data == "rlsolve":
+            print("Removing rejected links...")
+            os.system("python cnf_modifier.py -c \"{'rejected_links':[], 'state':'check'}\"")
+        elif data == "start":
+            print("Starting BOT...")
+            os.system("python cnf_modifier.py -c \"{'state':'start'}\"")
+        elif data == "stop":
+            print("Stopping BOT...")
+            os.system("python cnf_modifier.py -c \"{'state':'stop'}\"")
+        else:
+            print("Invalid command")
+
+
+if __name__ == '__main__':
+    setup();
